5548f23efba22225f1e6c56e52462cb73026dada
Asif Jahan <asifjahan307@gmail.com>
2024-04-28 21:37:40 +0600

    implemented user interface

--- weather_application/lib/Screen/home_page.dart
+++ weather_application/lib/Screen/home_page.dart
@@ -1,12 +1,13 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
+// ignore_for_file: unused_local_variable
 
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
-import 'package:jiffy/jiffy.dart';
+import 'package:geocoding/geocoding.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key});
@@ -15,93 +16,159 @@ class HomePage extends StatefulWidget {
   State createState() => _HomePageState();
 }
 
-class _HomePageState extends State {
-  Position? position;
+class _HomePageState extends State with WidgetsBindingObserver {
+  late Position _position;
+  late Map<String, dynamic> _weatherMap;
+  late Map<String, dynamic> _forecastMap;
+  late String _locationName = '';
 
-  determinePosition() async {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addObserver(this);
+    _determinePosition();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+  Future<void> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
+      _showErrorDialog('Location services are disabled.');
+      return;
     }
 
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+        _showErrorDialog('Location permissions are denied.');
+        return;
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      return Future.error(
+      _showErrorDialog(
           'Location permissions are permanently denied, we cannot request permissions.');
+      return;
     }
 
-    position = await Geolocator.getCurrentPosition();
-    setState(() {
-      latitude = position!.latitude;
-      longitude = position!.longitude;
-    });
-
-    fetchWeatherData();
+    try {
+      _position = await Geolocator.getCurrentPosition();
+      _fetchWeatherData();
+      _getLocationName(_position.latitude, _position.longitude);
+    } catch (e) {
+      _showErrorDialog('Error fetching location: $e');
+    }
   }
 
-  var latitude;
-  var longitude;
+  Future<void> _fetchWeatherData() async {
+    const String apiKey =
+        'IwAR0vlebaouEmURClRdtuWEg7qeBOdbDxmQ5HM9JDJL_mj5uDS7xqy4kCGTQ';
+    final double latitude = _position.latitude;
+    final double longitude = _position.longitude;
 
-  Map<String, dynamic>? weatherMap;
-  Map<String, dynamic>? forecastMap;
-
-  fetchWeatherData() async {
-    String WeatherUrl =
+    String weatherUrl =
         "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=f92bf340ade13c087f6334ed434f9761&fbclid=IwAR0vlebaouEmURClRdtuWEg7qeBOdbDxmQ5HM9JDJL_mj5uDS7xqy4kCGTQ";
-    String ForecastUrl =
+    String forecastUrl =
         "https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&units=metric&appid=f92bf340ade13c087f6334ed434f9761&fbclid=IwAR0vlebaouEmURClRdtuWEg7qeBOdbDxmQ5HM9JDJL_mj5uDS7xqy4kCGTQ";
 
-    var weatherResponse = await http.get(Uri.parse(WeatherUrl));
-    var forecastResponse = await http.get(Uri.parse(ForecastUrl));
-    weatherMap = Map<String, dynamic>.from(jsonDecode(weatherResponse.body));
-    forecastMap = Map<String, dynamic>.from(jsonDecode(forecastResponse.body));
-    setState(() {});
+    try {
+      final http.Response weatherResponse =
+          await http.get(Uri.parse(weatherUrl));
+      final http.Response forecastResponse =
+          await http.get(Uri.parse(forecastUrl));
 
-    if (kDebugMode) {
-      print("Weather Response is $latitude,$longitude");
+      _weatherMap = Map<String, dynamic>.from(jsonDecode(weatherResponse.body));
+      _forecastMap =
+          Map<String, dynamic>.from(jsonDecode(forecastResponse.body));
+
+      setState(() {});
+    } catch (e) {
+      _showErrorDialog('Error fetching weather data: $e');
     }
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    determinePosition();
-    super.initState();
+  Future<void> _getLocationName(double latitude, double longitude) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(latitude, longitude);
+      if (placemarks.isNotEmpty) {
+        Placemark placemark = placemarks[0];
+        String city = placemark.locality ??
+            placemark.subAdministrativeArea ??
+            placemark.administrativeArea ??
+            '';
+        setState(() {
+          _locationName = city;
+        });
+      }
+    } catch (e) {
+      print('Error getting location name: $e');
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xFF738BE3),
-        body: Row(
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.add,
-              color: Colors.white,
+    return Scaffold(
+      backgroundColor: const Color(0xFF738BE3),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                // const SizedBox(width: 40),
+                if (_locationName.isNotEmpty)
+                  Text(
+                    _locationName,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                const Icon(Icons.more_horiz_outlined),
+              ],
             ),
-            Text(
-              "${weatherMap!["name"]}",
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _determinePosition();
+    }
   }
 }
